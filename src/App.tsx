9fb270a754f3d6b0295efc17558a4f2c811b7bd9
ArianAngoma <arian.angoma@tecsup.edu.pe>
2021-12-05 10:37:12 -0500

    Consumo del product-card-arianjs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {ProductButtons, ProductImage, ProductTitle, ProductCard} from 'product-card-arianjs';
+
+const product = {
+    id: '1',
+    title: 'Caffe Mug - Card',
+    img: './coffee-mug.png'
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App App-header">
+            <ProductCard product={product} initialValues={{count: 5, maxCount: 15}}>
+
+                {
+                    ({reset, increaseBy, isMaxCountReached, count, maxCount}) => (
+                        <>
+                            <ProductImage/>
+                            <ProductTitle/>
+                            <ProductButtons/>
+                        </>
+                    )
+                }
+
+            </ProductCard>
+        </div>
+    );
 }
 
 export default App;
